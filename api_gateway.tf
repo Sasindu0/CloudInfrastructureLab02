f1a76102656d419dacb9017944bf83632565b99c
Sasindu <86899004+Sasindu0@users.noreply.github.com>
2023-12-15 20:03:32 +0530

    Update api_gateway.tf

--- api_gateway.tf
+++ api_gateway.tf
@@ -40,7 +40,7 @@ resource "aws_api_gateway_deployment" "my_api_deployment" {
   depends_on = [aws_api_gateway_integration.lambda_integration]
 
   rest_api_id = aws_api_gateway_rest_api.my_api.id
-  stage_name  = "dev" # stage name
+  stage_name  = "Terraform with Aws" # stage name
   lifecycle {
     create_before_destroy = true
   }
